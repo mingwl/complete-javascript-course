5a8af5f6e58aef8ba0eb1492f63ba3671b9c3c83
Li Jiang <lijiang8968@yahoo.com>
2026-05-06 12:18:55 +0200

    16  运算符

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,100 @@
+/*
 let js = 'amazing';
-if (js === 'amazing') alert('JS is fun')
 console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas');
+console.log(23)
+
+let firstName = 'Matilda';
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+// variable name
+let jonas_matilda = 'JM'
+let $function = 27
+let person = 'fd'
+let PI = 3.1415
+
+let myFirstJob = 'Coder'
+let myCurrentJob = 'teacher'
+
+console.log(myFirstJob)
+
+let jsIsFun = true
+console.log(jsIsFun)
+
+console.log( true)
+console.log( jsIsFun)
+// console.log( 23)
+// console.log( 'jonas')
+
+jsIsFun = 'yes'
+console.log( jsIsFun)
+
+let year;
+console.log(year)
+console.log( year)
+
+year = 1991;
+console.log(year)
+console.log( year)
+
+console.log( null)
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'fasd'
+console.log(lastName)
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+const firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+console.log(firstName + ' ' + lastName)
+
+let x = 10 + 5
+x += 10
+x *= 4
+x++
+x--
+x--
+console.log(x)
+
+// comparison operator
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+console.log(isFullAge)
+
+console.log(now - 1991 > now - 2018)
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018)
+console.log(25 - 10 - 5)
+
+let x, y
+x = y = 25 - 10 - 5
+console.log(x, y)
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge)
+*/
