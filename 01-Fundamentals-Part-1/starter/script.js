917c7399c900aa72039817f4a14296ae12683f92
Li Jiang <lijiang8968@yahoo.com>
2026-05-09 11:45:17 +0200

    21 falsy value

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -133,7 +133,6 @@ if (birthYear <= 2000) {
 }
 console.log(century)
 
-*/
 
 // type conversion
 const inputYear = '1991';
@@ -157,3 +156,25 @@ console.log('23' + '10' + 3)
 let n = '1' + 1 //11
 n = n - 1 //10
 console.log(n)
+*/
+
+// 5 falsy: 0, '', undefined, null, NaN
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 10;
+if (money) {
+    console.log(`Don't spend it all`)
+} else {
+    console.log(`You should get a job`)
+}
+
+let height = 0;
+if (height) {
+    console.log(`height is defined`)
+} else {
+    console.log(`Height is undefined`)
+}
