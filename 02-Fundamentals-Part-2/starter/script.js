7c32c13776cf46c411cfd293fe5c62663b6570a3
Li Jiang <lijiang8968@yahoo.com>
2026-05-10 11:33:45 +0200

    33 strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) {
+    console.log(`I can drive`)
+}
+
+// const interface = 'audio'
+// const private = 123
+// const if = 123
