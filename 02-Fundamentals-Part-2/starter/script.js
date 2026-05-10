be81433d2f1dd2ee35b1347e11c6c79c5f392f1f
Li Jiang <lijiang8968@yahoo.com>
2026-05-10 11:57:19 +0200

    34 函数

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -12,3 +13,25 @@ if (hasDriversLicense) {
 // const interface = 'audio'
 // const private = 123
 // const if = 123
+*/
+
+function logger() {
+    console.log(`My name is Jonas`);
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice
+}
+
+// console.log(fruitProcessor(2, 3));
+const appelJuice = fruitProcessor(5, 0);
+console.log(appelJuice);
+
+const appelOrangeJuice = fruitProcessor(2, 4);
+console.log(appelOrangeJuice)
