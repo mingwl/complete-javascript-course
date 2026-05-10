e7e09a84a65aae287bce280495a0875fd4595847
Li Jiang <lijiang8968@yahoo.com>
2026-05-10 21:06:28 +0200

    42 数组习题

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -163,7 +163,6 @@ console.log(age0, age1, ageLast);
 const ages = [calcAge(years2[0]), calcAge(years2[1]), calcAge(years2[years2.length - 1])];
 console.log(ages);
 
-*/
 
 const friends = ['Michael', 'Steven', 'Peter'];
 
@@ -198,3 +197,18 @@ if (friends.includes('Peter')) {
 if (friends.includes('Steven')) {
     console.log(`You have a friend called Steven`);
 }
+
+*/
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(calcTip(100));
+console.log(calcTip(1000));
+
+const bills = [125, 555, 44];
+console.log(bills);
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
