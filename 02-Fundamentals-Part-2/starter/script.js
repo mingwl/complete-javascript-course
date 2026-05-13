02e4d82bf69725d1fd6fdaf621fe0197d072a52d
Li Jiang <lijiang8968@yahoo.com>
2026-05-13 11:28:15 +0200

    46 对象方法习题

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -252,7 +252,6 @@ console.log(jonas);
 // chanllenge
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
 
-*/
 
 const jonas = {
     firstName: 'Jonas',
@@ -289,3 +288,37 @@ const jonas = {
 // Chanllenge
 console.log(jonas.getSummary());
 console.log(jonas.age);
+
+*/
+
+const mark = {
+    fullName: 'Mark Miller',
+    weight: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.weight / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+const john = {
+    fullName: 'John Smith',
+    weight: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.weight / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark);
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
+}
