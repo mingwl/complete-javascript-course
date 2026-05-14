b41b75bc6372386eb3617b8784e3b0b0aa3f8042
Li Jiang <lijiang8968@yahoo.com>
2026-05-14 10:55:38 +0200

    51 for循环习题

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -392,7 +392,6 @@ for (let i = 1; i <= 3; i++) {
     }
 }
 
-*/
 
 // 50
 
@@ -415,3 +414,30 @@ while (dice !== 6) {
     console.log(`${rep}: you rolled a ${dice === 6 ? '6!!!' : dice}`);
     rep++;
 }
+
+*/
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+let tip;
+for (let i = 0; i < bills.length; i++) {
+    tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = arr => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
