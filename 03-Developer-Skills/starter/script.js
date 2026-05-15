9ad5aa3e64e39299d49d8bd98d68bbef93f7cd06
Li Jiang <lijiang8968@yahoo.com>
2026-05-15 14:54:40 +0200

    61 calcTempAmplitude函数

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -9,19 +9,25 @@ const calcAge = birthYear => 2037 - birthYear;
 console.log(myvar);
 console.log(calcAge(1991));
 console.log(1951);
-*/
 
 const calcTempAmplitude = temps => {
   let min = temps[0];
   let max = temps[0];
-  for (let i = 1; i < temp.length; i++) {
-    if (typeof temps[i] !== 'number') continue;
-    min = temps[i] < min ? temps[i] : min;
-    max = temps[i] > max ? temps[i] : max;
-    console.log(min, max);
-    return max - min;
+  let cur;
+  for (let i = 1; i < temps.length; i++) {
+    cur = temps[i];
+    if (typeof cur !== 'number') continue;
+    min = cur < min ? cur : min;
+    max = cur > max ? cur : max;
   }
+  const amplitude = max - min;
+  console.log(`amplitude from ${min} to ${max} is ${amplitude}`);
+  return amplitude;
 };
 
 const temp = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 console.log(calcTempAmplitude(temp));
+console.log(calcTempAmplitude([3, 7, 4, 23]));
+
+const temp2 = [-100, 100];
+console.log(calcTempAmplitude(temp.concat(temp2)));
