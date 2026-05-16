1615334942d4efab3d14438716a681188deb73e4
Li Jiang <lijiang8968@yahoo.com>
2026-05-16 08:01:13 +0200

    63 debugger

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -31,3 +31,43 @@ console.log(calcTempAmplitude([3, 7, 4, 23]));
 
 const temp2 = [-100, 100];
 console.log(calcTempAmplitude(temp.concat(temp2)));
+
+*/
+
+const measureKelvin = () => {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsuis',
+    vallue: 10,
+    // value: Number(prompt(`Degrees Celsius: `)),
+  };
+
+  console.log(measurement);
+  //   console.table(measurement);
+  //   console.log(measurement.value, typeof measurement.value);
+  //   console.warn(measurement.value, typeof measurement.value);
+  //   console.error(measurement.value, typeof measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
+
+const calcTempAmplitude = temps => {
+  let min = temps[0];
+  let max = temps[0];
+  let cur;
+  for (let i = 1; i < temps.length; i++) {
+    cur = temps[i];
+    if (typeof cur !== 'number') continue;
+
+    debugger;
+    min = cur < min ? cur : min;
+    max = cur > max ? cur : max;
+  }
+  const amplitude = max - min;
+  console.log(`amplitude from ${min} to ${max} is ${amplitude}`);
+  return amplitude;
+};
+
+const temp = [3, 5, 1, 9, 4, 5];
+console.log(calcTempAmplitude(temp));
