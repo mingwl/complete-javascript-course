e752fddeea7b71773ef886d1c46643b183911a4b
Li Jiang <lijiang8968@yahoo.com>
2026-05-17 21:29:21 +0200

    90 猪游戏之重启游戏

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,8 +3,8 @@
 // selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-// const current0El = document.getElementById('current--0');
-// const current1El = document.getElementById('current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -12,14 +12,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// starting condition
-let activePlayer = 0;
-let currentScore = 0;
-let scores = [0, 0];
-let playing = true;
-score0El.textContent = currentScore;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let playing, activePlayer, currentScore, scores;
+const WIN_SCORE = 20;
+
+const init = () => {
+  // starting condition
+  playing = true;
+  activePlayer = 0;
+  currentScore = 0;
+  scores = [0, 0];
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player1El.classList.remove('player--winner');
+};
+init();
 
 const rollDice = () => Math.trunc(Math.random() * 6) + 1;
 
@@ -42,6 +54,8 @@ const switchPlayer = () => {
   player1El.classList.toggle('player--active');
 };
 
+btnNew.addEventListener('click', init);
+
 // holding score functionality
 btnHold.addEventListener('click', () => {
   if (!playing) return;
@@ -51,7 +65,7 @@ btnHold.addEventListener('click', () => {
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. check whether score >= 100
-  if (scores[activePlayer] >= 10) {
+  if (scores[activePlayer] >= WIN_SCORE) {
     // finish the game
     winTheGame();
   } else {
