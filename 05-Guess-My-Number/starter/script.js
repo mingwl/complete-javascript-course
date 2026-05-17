2f5639623208ed2ac93bcd6b1ac66a7d534c4121
Li Jiang <lijiang8968@yahoo.com>
2026-05-17 11:40:02 +0200

    82 猜数字重构

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -22,23 +22,49 @@ const body = document.querySelector('body');
 const bntCheck = document.querySelector('.check');
 const bntAgain = document.querySelector('.again');
 
-// 返回一个1到max之间的随机整数
-const getRandomNumber = max => Math.trunc(Math.random() * max) + 1;
 let secretNumber;
 let score;
 let highscore = 0;
 
+// 返回一个1到max之间的随机整数
+const getRandomNumber = max => Math.trunc(Math.random() * max) + 1;
+
+const displayMesage = msg => (labelMessage.textContent = msg);
+
+const decreaseScore = () => {
+  score--;
+  labelScore.textContent = score;
+};
+
+const guessRight = () => {
+  displayMesage('🎉答案正确');
+  labelSecretNumber.texctContent = secretNumber;
+  labelSecretNumber.style.width = '30rem';
+  body.style.backgroundColor = '#60b347';
+  highscore = score > highscore ? score : highscore;
+  labelHighscore.textContent = highscore;
+};
+
+const guessWrong = guess => {
+  if (score === 0) {
+    displayMesage('🔚游戏结束');
+    body.style.backgroundColor = '#b67c1f';
+  } else {
+    displayMesage(guess < secretNumber ? '⬆️数字太小' : '⬇️数字太大');
+  }
+};
+
 const init = () => {
   score = 20;
   secretNumber = getRandomNumber(20);
-  labelMessage.textContent = 'Start guessing...';
+  displayMesage('Start guessing...');
   inputNumber.value = '';
   labelScore.textContent = score;
   labelHighscore.textContent = highscore;
   labelSecretNumber.textContent = '?';
   labelSecretNumber.style.width = '15rem';
   body.style.backgroundColor = '#222';
-  labelSecretNumber.textContent = secretNumber;
+  // labelSecretNumber.textContent = secretNumber;
 };
 init();
 
@@ -50,39 +76,16 @@ bntCheck.addEventListener('click', function () {
 
   // 无输入
   if (!guess) {
-    labelMessage.textContent = '⚠️数字框不能为空';
+    displayMesage('⚠️数字框不能为空');
 
     // 答案正确
   } else if (guess === secretNumber) {
-    score--;
-    labelScore.textContent = score;
-    labelSecretNumber.textContent = secretNumber;
-    labelMessage.textContent = '🎉答案正确';
-    labelSecretNumber.style.width = '30rem';
-    body.style.backgroundColor = '#60b347';
-    highscore = score > highscore ? score : highscore;
-    labelHighscore.textContent = highscore;
+    decreaseScore();
+    guessRight();
 
-    // 数字太小
-  } else if (guess < secretNumber) {
-    score--;
-    labelScore.textContent = score;
-    if (score === 0) {
-      labelMessage.textContent = '🔚游戏结束';
-      body.style.backgroundColor = '#b67c1f';
-    } else {
-      labelMessage.textContent = '⬆️数字太小';
-    }
-
-    // 数字太大
+    // 数字错误
   } else {
-    score--;
-    labelScore.textContent = score;
-    if (score === 0) {
-      labelMessage.textContent = '🔚游戏结束';
-      body.style.backgroundColor = '#b67c1f';
-    } else {
-      labelMessage.textContent = '⬇️数字太大';
-    }
+    decreaseScore();
+    guessWrong(guess);
   }
 });
