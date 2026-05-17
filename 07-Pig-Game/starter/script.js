fa17c52fd9443319ecdd0ca32f50dbc0e68cfb76
Li Jiang <lijiang8968@yahoo.com>
2026-05-17 19:50:09 +0200

    88 猪游戏之切换玩家

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // selecting elements
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+// const current0El = document.getElementById('current--0');
+// const current1El = document.getElementById('current--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -13,8 +15,9 @@ const btnHold = document.querySelector('.btn--hold');
 const rollDice = () => Math.trunc(Math.random() * 6) + 1;
 
 // starting condition
+let activePlayer = 0;
 let currentScore = 0;
-let score = 0;
+let scores = [0, 0];
 score0El.textContent = currentScore;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -30,16 +33,25 @@ btnRoll.addEventListener('click', () => {
   diceEl.src = `dice-${dice}.png`;
 
   // 3. check whether dice roll is 1
+  // debugger;
   if (dice === 1) {
-    // 4. if true switch to next player
-    score += currentScore;
+    // 4. if true
+    // keep score for current player
+    scores[activePlayer] += currentScore;
     currentScore = 0;
-    score0El.textContent = score;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   } else {
-    // 5. else display dice roll to current score
-    currentScore += dice;
-    current0El.textContent = currentScore;
+    // 5. display dice roll
     // 6. display new score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   }
 });
