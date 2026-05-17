43fb527a23ed99609555fe7fd697a2164bdb3d63
Li Jiang <lijiang8968@yahoo.com>
2026-05-17 11:06:26 +0200

    81 猜数字最高纪录

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,6 +16,7 @@ console.log(document.querySelector('.guess').value);
 const labelMessage = document.querySelector('.message');
 const labelSecretNumber = document.querySelector('.number');
 const labelScore = document.querySelector('.score');
+const labelHighscore = document.querySelector('.highscore');
 const inputNumber = document.querySelector('.guess');
 const body = document.querySelector('body');
 const bntCheck = document.querySelector('.check');
@@ -25,16 +26,18 @@ const bntAgain = document.querySelector('.again');
 const getRandomNumber = max => Math.trunc(Math.random() * max) + 1;
 let secretNumber;
 let score;
+let highscore = 0;
 
 const init = () => {
   score = 20;
   secretNumber = getRandomNumber(20);
-  labelSecretNumber.textContent = '?';
   labelMessage.textContent = 'Start guessing...';
+  inputNumber.value = '';
   labelScore.textContent = score;
+  labelHighscore.textContent = highscore;
+  labelSecretNumber.textContent = '?';
   labelSecretNumber.style.width = '15rem';
   body.style.backgroundColor = '#222';
-  inputNumber.value = '';
   labelSecretNumber.textContent = secretNumber;
 };
 init();
@@ -57,6 +60,8 @@ bntCheck.addEventListener('click', function () {
     labelMessage.textContent = '🎉答案正确';
     labelSecretNumber.style.width = '30rem';
     body.style.backgroundColor = '#60b347';
+    highscore = score > highscore ? score : highscore;
+    labelHighscore.textContent = highscore;
 
     // 数字太小
   } else if (guess < secretNumber) {
