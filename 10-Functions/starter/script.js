96f046399d9c63a3309a39f31c5cb24ad05b33ef
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 19:29:21 +0200

    136 函数数值与引用参数

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+// 136 函数数值与引用参数
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schemdtmann',
+  passport: 1234567890,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 1234567890) {
+    alert(`Check in`);
+  } else {
+    alert(`Wrong passport`);
+  }
+};
+// checkIn(flight, jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+console.log(flight, jonas);
+newPassport(jonas);
+console.log('after newPassport:', jonas);
+checkIn(flight, jonas);
+
+/*
 // 135 函数默认参数
 const bookings = [];
 
@@ -26,3 +54,4 @@ createBooking('LH123', 2);
 createBooking('LH123', 3);
 createBooking('LH123', undefined, 600);
 createBooking('LH123', 2, 800);
+*/
