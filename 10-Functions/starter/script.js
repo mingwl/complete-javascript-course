536674e49ef506803638396c9e36b99936aaaa04
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 20:42:49 +0200

    138 接收回调函数的函数

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+// 138 接收回调函数的函数
+const oneWord = function (str) {
+  // return str.replaceAll(' ', '');
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// 高阶函数 higher order function: transformer
+// 回调函数 called backed function：upperFirstWord/oneWord
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+// higher order function: addEventListener
+// called backed function: high5
+const high5 = function () {
+  console.log(`🙌`);
+};
+document.body.addEventListener('click', high5);
+
+// higher order function: forEach
+// called backed function: high5
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+/*
 // 136 函数数值与引用参数
 const flight = 'LH234';
 const jonas = {
@@ -27,7 +60,6 @@ newPassport(jonas);
 console.log('after newPassport:', jonas);
 checkIn(flight, jonas);
 
-/*
 // 135 函数默认参数
 const bookings = [];
 
