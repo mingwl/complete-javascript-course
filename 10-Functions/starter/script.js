f658a41702985aa07687dc6a2a31aa9427df55af
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 18:29:25 +0200

    135 函数默认参数

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// 135 函数默认参数
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 100 * numPassengers,
+) {
+  // ES5
+  // numPassengers ||= 1;
+  // price ||= 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2);
+createBooking('LH123', 3);
+createBooking('LH123', undefined, 600);
+createBooking('LH123', 2, 800);
