158a07d810ee0d8553031c44f3933d274175e780
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 09:03:39 +0200

    127 数据结构习题3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -101,6 +101,43 @@ const game = {
   },
 };
 
+// 127 数据结构习题3
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`,
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+/*
 // 125 Map循环
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
@@ -136,7 +173,6 @@ const answer = 3;
 console.log(answer);
 console.log(question.get(question.get('correct') === answer));
 
-/*
 // 124 Map ES6
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
