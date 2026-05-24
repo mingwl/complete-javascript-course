7c74b172ea7e5a5130649725a7dcbc9d4b44b222
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 16:46:58 +0200

    131 数据结构习题3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -101,6 +101,54 @@ const game = {
   },
 };
 
+// 131 数据结构习题3
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.trim().toLowerCase().split('_');
+    // console.log(
+    //   [first, second[0].toUpperCase() + second.slice(1)]
+    //     .join('')
+    //     .padEnd(20, ' '),
+    // );
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 2)}`);
+  }
+});
+
+/*
+// Coding Challenge #4
+
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
 // 130 处理字符串3
 console.log('a+very+nice+string'.split('+'));
 console.log('Jonas Schmedtmann'.split(' '));
@@ -149,7 +197,6 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(10);
 
-/*
 // 129 处理字符串2
 const airline = 'TAP Air Portugal';
 console.log(airline.toLowerCase());
