3aec5a5e5ad2f669b55efe53182032880afc2f0c
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 08:31:40 +0200

    125 Map循环

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -101,6 +101,41 @@ const game = {
   },
 };
 
+// 125 Map循环
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// convert map to array
+console.log([...hoursMap]);
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+// map are iterables: Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key !== 'number') continue;
+  console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
 /*
 // 124 Map ES6
 const rest = new Map();
