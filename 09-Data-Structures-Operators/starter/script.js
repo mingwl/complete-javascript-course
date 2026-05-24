b511e981c86094e3c6c57e206558a4288bbd8d1f
Li Jiang <lijiang8968@yahoo.com>
2026-05-24 07:16:40 +0200

    124 Map ES6

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -101,6 +101,39 @@ const game = {
   },
 };
 
+/*
+// 124 Map ES6
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time >= rest.get('open') && time <= rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
 const italianFoods = new Set([
   'pasta',
   'gnocchi',
@@ -146,7 +179,6 @@ console.log(
   italianFoods.isDisjointFrom(mexicanFoods),
 );
 
-/*
 // 122 Set (iterable)
 // param is also an iterable (string, array)
 const orderSet = new Set([
