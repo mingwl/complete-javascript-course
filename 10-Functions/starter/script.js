3c0b8cee069108b0c61271476b8f29d7413170cb
Li Jiang <lijiang8968@yahoo.com>
2026-05-25 10:25:37 +0200

    142: 函数习题1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,86 @@
 'use strict';
 
+// 142: 函数习题1
+///////////////////////////////////////
+// Coding Challenge #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const str = [this.question, ...this.options, '(Write option number)'].join(
+      '\n',
+    );
+    const answer = Number(prompt(str));
+    if (Number.isNaN(answer)) {
+      alert(`answer must be a number`);
+      return;
+    }
+    if (answer < 0 || answer > 3) {
+      alert(`answer must between 0 and 3`);
+      return;
+    }
+    this.answers[answer]++;
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    type === 'array' && console.log(this.answers);
+    type === 'string' &&
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+  },
+};
+// poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+console.log(`--- call ---`);
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+
+console.log(`--- apply ---`);
+poll.displayResults.apply({ answers: [1, 5, 3, 9, 6, 1] });
+poll.displayResults.apply({ answers: [1, 5, 3, 9, 6, 1] }, ['string']);
+
+console.log(`--- bind ---`);
+const displayResults1 = poll.displayResults.bind({
+  answers: [5, 2, 3, 4, 5],
+});
+displayResults1();
+displayResults1('string');
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+
 // 140 重置this关键词(call/apply)
 const lufthansa = {
   airline: 'Lufthansa',
@@ -95,7 +176,6 @@ const addVAT023 = addTaxRate(0.23);
 console.log('addVAT023', addVAT023(100));
 console.log('addVAT023', addVAT023(23));
 
-/*
 // 139 返回新函数的高阶函数
 // 函数表达式
 const greet = function (greeting) {
