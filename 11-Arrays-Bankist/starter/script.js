a1a2eeb6199cbeeaae677a873cda8e636a182410
Li Jiang <lijiang8968@yahoo.com>
2026-05-26 06:51:50 +0200

    149 简单数组方法

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,37 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// 149 简单数组方法
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(`不作用于原始数组的切片 slice`);
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+console.log(`作用于原始数组上切片 splice`);
+// console.log(arr.splice(2));
+console.log(arr.splice(-1));
+console.log(arr);
+console.log(arr.splice(1, 2));
+console.log(arr);
+
+console.log(`作用于原始数组上倒序 reverse`);
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+console.log(`不作用于原始数组上连接 concat`);
+arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(`concat`);
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+console.log(`join`);
+console.log(letters.join(' - '));
