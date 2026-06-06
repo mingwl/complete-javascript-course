e713219659865e5ec7a8c57d5e048fc6eb222cf8
Li Jiang <lijiang8968@yahoo.com>
2026-06-06 21:14:30 +0200

    184 数字分割运算符ES2021

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -255,6 +255,27 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// 184 数字分割运算符ES2021
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee1, transferFee2);
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230000'));
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
+/*
 // 182 余数运算符 remainder operator
 console.log(5 % 2);
 console.log(5 / 2);
@@ -281,7 +302,6 @@ labelBalance.addEventListener('click', function () {
   });
 });
 
-/*
 // 182 数学与四舍五入函数
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
